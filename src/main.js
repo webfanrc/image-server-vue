d001dcf2cd6fbbb03a165c28f7e40c7711035439
RuoChen95 <xrc_1995@163.com>
2020-06-05 19:55:03 +0800

    部署准备

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+if ( process.env.NODE_ENV === 'development' ) {
+  // 本地环境
+  axios.defaults.baseURL="http://localhost:4001";
+} else {
+  // 线上环境
+  axios.defaults.baseURL="";
+}
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
